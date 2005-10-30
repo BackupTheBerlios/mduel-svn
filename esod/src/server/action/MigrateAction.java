29ab67cfe0d418fb23c8cbe0d42fc725fa60aaec
mgcm <mgcm@25a59b7b-a1ed-0310-b96d-d87e407d5862>
2005-10-30 01:44:09 +0000

    forgot this little puppy

---
+++ esod/src/server/action/MigrateAction.java
@@ -0,0 +1,22 @@
+package server.action;
+
+import java.rmi.RemoteException;
+import server.agent.Agent;
+
+public class MigrateAction implements Action  {
+	private static final long serialVersionUID = -5809777582121076251L;
+	private String hostname;
+
+	public MigrateAction(String newHostname) {
+		this.hostname = newHostname;
+	}
+	
+	public void run(Agent agent) {
+		try {
+			agent.getHome().moveTo(agent, hostname);
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
