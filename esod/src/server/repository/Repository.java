58e460668be3cec3ffefd2f63d51207199d5c44a
mgcm <mgcm@25a59b7b-a1ed-0310-b96d-d87e407d5862>
2005-11-03 02:06:02 +0000

    teste

--- esod/src/server/repository/Repository.java
+++ esod/src/server/repository/Repository.java
@@ -1,10 +1,20 @@
 package server.repository;
 
 import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.LinkedList;
 
-import server.agent.Agent;
+import server.AgentHost;
+import server.action.Action;
 
 public interface Repository extends Remote {
-	public int setInfo(String agentID, String agentHostID, Object action, String output);
-	public AgentData readInfo(String agentID, String agentHostID);
+	
+	
+	public void setActionReport(Object agentID, Action task) throws RemoteException;
+	public void setHostReport(Object agentID, AgentHost host, LinkedList tasks) throws RemoteException;
+
+	public LinkedList getHostReport(Object agentID, AgentHost host) throws RemoteException;
+	public AgentReport getFinalReport(Object agentID) throws RemoteException;
+
+
 }
